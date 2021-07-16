3d0ce5b02ec53fc5a8e1e091b8e98eecd8741c4e
Larry Garfield <larry@garfieldtech.com>
2021-07-16 15:27:40 -0500

    Refactor LoaderTest to use dataProviders more.

--- tests/LoaderTest.php
+++ tests/LoaderTest.php
@@ -8,6 +8,7 @@ use Crell\Rekodi\Records\MultiKey;
 use Crell\Rekodi\Records\Person;
 use Crell\Rekodi\Records\PersonSSN;
 use Crell\Rekodi\Records\Point;
+use Doctrine\DBAL\Connection;
 use \PHPUnit\Framework\TestCase;
 
 class LoaderTest extends TestCase
@@ -22,117 +23,117 @@ class LoaderTest extends TestCase
 
     /**
      * @test
+     * @dataProvider objectSaveProvider
      */
-    public function can_save_objects(): void
+    public function can_save_objects(string $class, array $records, callable $tests): void
     {
         $conn = $this->getConnection();
-        $this->ensureTableClass(Point::class);
+        $this->ensureTableClass($class);
 
         $loader = new Loader($conn);
 
-        $p = new Point(x: 5, y: 6, z: 7);
+        foreach ($records as $record) {
+            $loader->save($record);
+        }
 
-        $loader->save($p);
-
-        $result = $conn->executeQuery("SELECT x, y_axis, z FROM MyPoints WHERE x=?", [5]);
-        $data = $result->fetchAssociative();
-        self::assertEquals($p->x, $data['x']);
-        self::assertEquals($p->y, $data['y_axis']);
-        self::assertEquals($p->z, $data['z']);
+        $tests($conn, $loader);
     }
 
     /**
-     * @test
+     * @see can_save_objects()
      */
-    public function can_load_objects(): void
+    public function objectSaveProvider(): iterable
     {
-        $conn = $this->getConnection();
-        $this->ensureTableClass(Point::class);
-
-        $loader = new Loader($conn);
-
-        $loader->save(new Point(x: 1, y: 2, z: 3));
-        $loader->save(new Point(x: 4, y: 5, z: 6));
-        $loader->save(new Point(x: 1, y: 8, z: 9));
-
-        $result = $conn->executeQuery("SELECT x, y_axis, z FROM MyPoints WHERE x=? ORDER BY x, y_axis, z", [1]);
-        $records = iterator_to_array($loader->loadRecords(Point::class, $result));
-        self::assertEquals(new Point(x: 1, y: 2, z: 3), $records[0]);
-        self::assertEquals(new Point(x: 1, y: 8, z: 9), $records[1]);
-    }
-
-    /**
-     * @test
-     */
-    public function can_save_new_record_with_no_id(): void
-    {
-        $conn = $this->getConnection();
-        $this->ensureTableClass(Person::class);
-
-        $loader = new Loader($conn);
-
-        // Assume the generated ID is 1, because the table is fresh. We hope.
-        $loader->save(new Person(first: 'Larry', last: 'Garfield'));
-
-        $result = $conn->executeQuery("SELECT id, first, last FROM Person");
-        $loaded = iterator_to_array($loader->loadRecords(Person::class, $result))[0];
-        self::assertEquals(1, $loaded->id);
-        self::assertEquals('Larry', $loaded->first);
-        self::assertEquals('Garfield', $loaded->last);
-    }
-
-    /**
-     * @test
-     */
-    public function can_save_and_load_by_autoinc_id(): void
-    {
-        $conn = $this->getConnection();
-        $this->ensureTableClass(Person::class);
-
-        $loader = new Loader($conn);
-
-        // Assume the generated ID is 1, because the table is fresh. We hope.
-        $loader->save(new Person(first: 'Larry', last: 'Garfield'));
-
-        $person = $loader->load(Person::class, 1);
-
-        self::assertEquals('Larry', $person->first);
-        self::assertEquals('Garfield', $person->last);
-        self::assertIsInt($person->id);
-        self::assertSame($person->id, 1);
-    }
-
-    /**
-     * @test
-     */
-    public function can_save_and_load_by_string_id(): void
-    {
-        $conn = $this->getConnection();
-        $this->ensureTableClass(PersonSSN::class);
-
-        $loader = new Loader($conn);
-
-        $loader->save(new PersonSSN(ssn: '123-45-6789', first: 'Larry', last: 'Garfield'));
-
-        $person = $loader->load(PersonSSN::class, '123-45-6789');
-
-        self::assertEquals('Larry', $person->first);
-        self::assertEquals('Garfield', $person->last);
-        self::assertEquals('123-45-6789', $person->ssn);
-    }
-
-    public function test_load_missing_record(): void
-    {
-        $conn = $this->getConnection();
-        $this->ensureTableClass(PersonSSN::class);
-
-        $loader = new Loader($conn);
-
-        $loader->save(new PersonSSN(ssn: '123-45-6789', first: 'Larry', last: 'Garfield'));
-
-        $ret = $loader->load(PersonSSN::class, '987-654-321');
-
-        self::assertNull($ret);
+        yield 'Basic save with custom table name' => [
+            'class' => Point::class,
+            'records' => [
+                new Point(x: 5, y: 6, z: 7),
+            ],
+            'test' => function(Connection $conn, Loader $loader) {
+                $result = $conn->executeQuery("SELECT x, y_axis, z FROM MyPoints WHERE x=?", [5]);
+                $data = $result->fetchAssociative();
+                self::assertEquals(5, $data['x']);
+                self::assertEquals(6, $data['y_axis']);
+                self::assertEquals(7, $data['z']);
+            },
+        ];
+        yield 'Non-Id objects' => [
+            'class' => Point::class,
+            'records' => [
+                new Point(x: 1, y: 2, z: 3),
+                new Point(x: 4, y: 5, z: 6),
+                new Point(x: 1, y: 8, z: 9),
+            ],
+            'test' => function(Connection $conn, Loader $loader) {
+                $result = $conn->executeQuery("SELECT x, y_axis, z FROM MyPoints WHERE x=? ORDER BY x, y_axis, z", [1]);
+                $records = iterator_to_array($loader->loadRecords(Point::class, $result));
+                self::assertEquals(new Point(x: 1, y: 2, z: 3), $records[0]);
+                self::assertEquals(new Point(x: 1, y: 8, z: 9), $records[1]);
+            },
+        ];
+        yield 'Auto-increment-id objects (manual)' => [
+            'class' => Person::class,
+            'records' => [
+                new Person(first: 'Larry', last: 'Garfield'),
+            ],
+            'test' => function(Connection $conn, Loader $loader) {
+                $result = $conn->executeQuery("SELECT id, first, last FROM Person");
+                $loaded = iterator_to_array($loader->loadRecords(Person::class, $result))[0];
+                self::assertEquals(1, $loaded->id);
+                self::assertEquals('Larry', $loaded->first);
+                self::assertEquals('Garfield', $loaded->last);
+            },
+        ];
+        yield 'Auto-increment-id objects' => [
+            'class' => Person::class,
+            'records' => [
+                new Person(first: 'Larry', last: 'Garfield'),
+            ],
+            'test' => function(Connection $conn, Loader $loader) {
+                $person = $loader->load(Person::class, 1);
+                self::assertNotNull($person);
+                self::assertEquals('Larry', $person->first);
+                self::assertEquals('Garfield', $person->last);
+                self::assertIsInt($person->id);
+                self::assertSame($person->id, 1);
+            },
+        ];
+        yield 'String-id objects' => [
+            'class' => PersonSSN::class,
+            'records' => [
+                new PersonSSN(ssn: '123-45-6789', first: 'Larry', last: 'Garfield'),
+            ],
+            'test' => function(Connection $conn, Loader $loader) {
+                $person = $loader->load(PersonSSN::class, '123-45-6789');
+                self::assertNotNull($person);
+                self::assertEquals('Larry', $person->first);
+                self::assertEquals('Garfield', $person->last);
+                self::assertEquals('123-45-6789', $person->ssn);
+            },
+        ];
+        yield 'Missing record' => [
+            'class' => PersonSSN::class,
+            'records' => [
+                new PersonSSN(ssn: '123-45-6789', first: 'Larry', last: 'Garfield'),
+            ],
+            'test' => function(Connection $conn, Loader $loader) {
+                $ret = $loader->load(PersonSSN::class, '987-654-321');
+                self::assertNull($ret);
+            },
+        ];
+        yield 'Multi-key record' => [
+            'class' => MultiKey::class,
+            'records' => [
+                new MultiKey(scope: 4, localId: 5, data: 'beep'),
+            ],
+            'test' => function(Connection $conn, Loader $loader) {
+                $record = $loader->load(MultiKey::class, scope: 4, localId: 5);
+                self::assertNotNull($record);
+                self::assertEquals(4, $record->scope);
+                self::assertEquals(5, $record->localId);
+                self::assertEquals('beep', $record->data);
+            },
+        ];
     }
 
     /**
@@ -151,35 +152,47 @@ class LoaderTest extends TestCase
         }
 
         foreach ($deleteIds as $del) {
-            $loader->delete($class, $del);
+            if (is_array($del)) {
+                $loader->delete($class, ...$del);
+            } else {
+                $loader->delete($class, $del);
+            }
         }
 
         foreach ($deleteIds as $del) {
-            self::assertNull($loader->load($class, $del));
+            if (is_array($del)) {
+                self::assertNull($loader->load($class, ...$del));
+            } else {
+                self::assertNull($loader->load($class, $del));
+            }
         }
     }
 
     /**
-     * @test
+     * @see can_delete_by_id()
      */
-    public function can_save_load_and_delete_compound_keys(): void
+    public function deletionDataProvider(): iterable
     {
-        $conn = $this->getConnection();
-        $this->ensureTableClass(MultiKey::class);
-
-        $loader = new Loader($conn);
-
-        $loader->save(new MultiKey(scope: 4, localId: 5, data: 'beep'));
-
-        $record = $loader->load(MultiKey::class, scope: 4, localId: 5);
-
-        self::assertEquals(4, $record->scope);
-        self::assertEquals(5, $record->localId);
-        self::assertEquals('beep', $record->data);
-
-        $loader->delete(MultiKey::class, scope: 4, localId: 5);
-
-        self::assertNull($loader->load(MultiKey::class, scope: 4, localId: 5));
+        yield 'Single string ID field' => [
+            'class' => PersonSSN::class,
+            'records' => [
+                new PersonSSN('123-45-6789', 'Larry', 'Garfield'),
+                new PersonSSN('321-65-4987', 'Jimmy', 'Garfield'),
+            ],
+            'deleteIds' => [
+                '123-45-6789',
+                '321-65-4987',
+            ]
+        ];
+        yield 'Multi-key string ID fields' => [
+            'class' => MultiKey::class,
+            'records' => [
+                new MultiKey(scope: 4, localId: 6, data: 'beep'),
+            ],
+            'deleteIds' => [
+                ['scope' => 4, 'localId' => 6],
+            ]
+        ];
     }
 
     /**
@@ -213,7 +226,7 @@ class LoaderTest extends TestCase
 
         $loader->save(new MultiKey(scope: 4, localId: 5, data: 'beep'));
 
-        $loader->load(MultiKey::class, scope: 4, local: 5, data: 'beep');
+        $loader->load(MultiKey::class, scope: 4, localId: 5, data: 'beep');
     }
 
     /**
@@ -231,21 +244,6 @@ class LoaderTest extends TestCase
         $loader->save(new MultiKey(scope: 4, localId: 5, data: 'beep'));
 
         $loader->load(MultiKey::class, ...[4, 5]);
-    }
-
-    public function deletionDataProvider(): iterable
-    {
-        yield [
-            'class' => PersonSSN::class,
-            'records' => [
-                new PersonSSN('123-45-6789', 'Larry', 'Garfield'),
-                new PersonSSN('321-65-4987', 'Jimmy', 'Garfield'),
-            ],
-            'deleteIds' => [
-                '123-45-6789',
-                '321-65-4987',
-            ]
-        ];
     }
 
     protected function ensureTableClass(string $class): void
